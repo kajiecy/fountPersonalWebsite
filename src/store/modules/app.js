43b77be192429d9d5fee378d143070e619419958
kajiecy <851416347@qq.com>
2018-06-05 18:23:56 +0800

    使用新版vue进行构建

---
+++ src/store/modules/app.js
@@ -0,0 +1,14 @@
+// import {routers,appRouter} from '@/router/router';
+
+const app = {
+    state: {
+      menuTheme: 'dark', // 主题
+      openedSubmenuArr: [], // 要展开的菜单数组
+      menuList: [],
+    },
+    mutations: {
+
+    }
+};
+
+export default app;
---
+++ src/views/layout/KajieHeader.vue
@@ -0,0 +1,32 @@
+<template>
+    <el-header>
+        header
+    </el-header>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {}
+        },
+        created() {
+
+        },
+        mounted() {
+
+        },
+        mounted() {
+
+        },
+        watch: {},
+        methods: {},
+        computed: {},
+        components: {}
+    }
+</script>
+
+<style scoped>
+    .el-header{
+        background-color: #D1D1D1;
+    }
+</style>
